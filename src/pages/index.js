d0a54a929c144bea1780f2dcafe1292a64ee1019
Federico Torresan <federico.torresan@quentral.com>
2021-10-27 13:47:20 +0200

    removed unused button

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,11 +14,11 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-            <Link
-                className="button button--primary button--lg"
-                to="/docs/api/intro">
-                Build a template
-            </Link>
+            {/*<Link*/}
+            {/*    className="button button--primary button--lg"*/}
+            {/*    to="/docs/api/intro">*/}
+            {/*    Build a template*/}
+            {/*</Link>*/}
             <Link
             className="button button--primary button--lg"
             to="/docs/api/intro">
