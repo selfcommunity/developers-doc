08851797ee4df035ff1b572e3836f5666f702bc5
Simona Melis <simona.melis@quentral.com>
2022-03-07 13:27:28 +0100

    fix: update packages, added homepage section for sdk

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -3,50 +3,62 @@ import clsx from 'clsx';
 import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
-  {
-    title: 'API Documentation',
-    Svg: require('../../static/img/api.svg').default,
-    description: (
-      <>
-        Explore our APIs. Learn how to build apps and integrations with the SelfCommunity platform
-      </>
-    ),
-  },
-  {
-    title: 'API Reference',
-    Svg: require('../../static/img/api_reference.svg').default,
-    description: (
-      <>
-          Harness the power of our v2 APIs. Discover the endpoints you can use to build innovative community solutions.
-      </>
-    ),
-  },
+    {
+        title: 'API Documentation',
+        Svg: require('../../static/img/api.svg').default,
+        description: (
+            <>
+                Explore our APIs. Learn how to build apps and integrations with the SelfCommunity platform
+            </>
+        ),
+    },
+    {
+        title: 'API Reference',
+        Svg: require('../../static/img/api_reference.svg').default,
+        description: (
+            <>
+                Harness the power of our v2 APIs. Discover the endpoints you can use to build innovative community
+                solutions.
+            </>
+        ),
+    },
+    {
+        title: 'Community-UI SDK',
+        Svg: require('../../static/img/api.svg').default,
+        description: (
+            <>
+                Quickly embed beautiful community with SelfCommunity.
+                A set of React components for integrating an empowered Social Networking Technology Community in your
+                frontend.
+            </>
+        ),
+    },
 ];
 
 function Feature({Svg, title, description}) {
-  return (
-    <div className={clsx('col col--6')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} alt={title} />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className={clsx('col col-xs-6')}>
+            <div className="text--center">
+                <Svg className={styles.featureSvg} alt={title}/>
+            </div>
+            <div className="text--center padding-horiz--md">
+                <h3>{title}</h3>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
 }
 
 export default function HomepageFeatures() {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                <div className="row">
+                    {FeatureList.map((props, idx) => (
+                        <Feature key={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
